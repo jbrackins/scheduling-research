02cdba9e7acb99ed56b8a88207547b8ccf39b674
Julian Brackins <julian.brackins@mines.sdsmt.edu>
2016-10-23 17:34:20 -0600

    classrooms.py file is a dictionary containing information related to all classrooms

---
+++ src/classrooms.py
@@ -0,0 +1,20 @@
+code = {}
+
+# dict containing all the classrooms by code
+code["MPL"]  = "Paleontology Laboratories"
+code["RCUC"] =  "Rapid City University Center"
+code["MMC"]  = "Music Center"
+code["MKC"] = "King Center"
+code["MC"] = "Chem-Chem Engr Building"
+code["MIER"] = "Industrial Engineering Researc"
+code["MEP"] = "Electrical Engr-Physics"
+code["MMI"] = "Mineral Industries Buildi"
+code["MCB"] = "Classroom Building"
+code["COFC"] = "Off Campus"
+code["MOG"] = "Music Center"
+code["MCBC"] = "Chemical-Bio-Chemistry"
+code["MCM"] = "Civil-Mechanical-Engr-Bldg"
+code["MM"] = "Mc Laury Building"
+code["MNG"] = "Physical Education Center"
+
+# add more here.
